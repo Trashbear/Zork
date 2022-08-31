002c38ae780d0bf3306c1292c66eac1270270c9f
Michael Rodriguez <mikey8200@gmail.com>
2022-08-31 17:24:11 -0400

    Assignment 1.2 First Commit

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -6,22 +6,15 @@ namespace Zork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Zork!");
+            Console.WriteLine("Welome to Zork!");
+            string inputString = Console.ReadLine().Trim();
+            Commands command = ToCommand(inputString);
+            Console.WriteLine(command);
+        }
 
-            string inputString = Console.ReadLine().Trim().ToUpper();
-
-            if (inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine($"Unkown Command: {inputString}");
-            }
+        static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.Unknown;
         }
     }
 }
