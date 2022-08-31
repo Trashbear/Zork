a7e86a27ba3eb04a175dd9754e0ca46185234f44
Michael Rodriguez <mikey8200@gmail.com>
2022-08-31 17:25:57 -0400

    Assignment 1.3 First Commit

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,14 +7,44 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welome to Zork!");
-            string inputString = Console.ReadLine().Trim();
-            Commands command = ToCommand(inputString);
-            Console.WriteLine(command);
+
+            bool isRunning = true;
+
+            while (isRunning == true)
+            {
+                Console.Write("> ");
+                string inputString = Console.ReadLine().Trim();
+                Commands command = ToCommand(inputString);
+                string outputString;
+                switch (command)
+                {
+                    case Commands.Quit:
+                        isRunning = false;
+                        outputString = "Thank you for playing!";
+                        break;
+                    case Commands.Look:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+                    case Commands.North:
+                    case Commands.South:
+                    case Commands.East:
+                    case Commands.West:
+                        outputString = $"You moved {command}.";
+                        break;
+                    default:
+                        outputString = "Unknown Command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
+
         }
 
         static Commands ToCommand(string commandString)
         {
             return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.Unknown;
         }
+
     }
 }
